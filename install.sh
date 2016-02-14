c96501c4def58a20becc073a42005c1c1254e3fa
xcv58 <chenyihonglove@gmail.com>
2016-02-14 11:09:13 -0500

    setup db path in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DB_PATH="/data/db"
 install() {
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
     echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
@@ -7,7 +8,17 @@ install() {
     sudo apt-get install -y mongodb-org
 }
 
+setup_path() {
+    sudo mkdir -p ${DB_PATH}
+    sudo chown -R mongodb:mongodb ${DB_PATH}
+}
+
 command -v mongo >/dev/null 2>&1 || install
+
+if [ ! -d "${DB_PATH}" ]; then
+    setup_path
+fi
+
 
 CONFIG="./configs/mongod.yaml"
 CONFIG_TARGET="/etc/mongod.conf"
